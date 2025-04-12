198d3cbd2a1a97059c5c7cb494509ea88df668b0
Brynja <bkb17@hi.is>
2025-04-12 17:15:42 +0000

    bjó til Bird og BirdSelection

---
+++ src/main/java/hi/flappybird/vidmot/BirdSelectionController.java
@@ -0,0 +1,32 @@
+package hi.flappybird.vidmot;
+
+import hi.flappybird.vinnsla.SelectedBird;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BirdSelectionController {
+
+    @FXML
+    private void selectPinkBird(ActionEvent event) throws Exception {
+        SelectedBird.setSelectedBird("pink");
+        goToMainMenu(event);
+    }
+
+    @FXML
+    private void selectBlueBird(ActionEvent event) throws Exception {
+        SelectedBird.setSelectedBird("blue");
+        goToMainMenu(event);
+    }
+
+    private void goToMainMenu(ActionEvent event) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("main-menu.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+    }
