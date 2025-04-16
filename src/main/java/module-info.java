a20291d0e418f112dd9a71090aa1e7da5e25ce81
sgh22 <sgh22@hi.is>
2025-04-16 23:15:25 +0000

    auka

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module hi.flappybird {
 
 
     opens hi.flappybird to javafx.fxml;
+    exports hi.flappybird;
     exports hi.flappybird.vinnsla;
     opens hi.flappybird.vinnsla to javafx.fxml;
     exports hi.flappybird.vidmot;
